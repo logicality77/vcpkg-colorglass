a6c4715cd78fb14734bdd48e49d20e85374465e8
nikitalita <69168929+nikitalita@users.noreply.github.com>
2022-11-26 21:39:34 -0800

    Update and fix skse

--- ports/skse/portfile.cmake
+++ ports/skse/portfile.cmake
@@ -1,12 +1,16 @@
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         FEATURES
-        "plugin"       BUILD_PLUGIN
-        "scripts"      INSTALL_SCRIPTS
+        "plugin"              BUILD_PLUGIN
+        "scripts"             INSTALL_SCRIPTS
+        "internal-xbyak"      INSTALL_INTERNAL_XBYAK
         )
 
 if (BUILD_PLUGIN)
   if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
     message(WARNING "Warning: SKSE only supports static linkage, overriding setting.")
+  endif ()
+  if (VCPKG_CRT_LINKAGE STREQUAL dyanmic)
+    message(WARNING "Warning: SKSE only supports static linkage against CRT, overriding setting.")
   endif ()
   set(VCPKG_LIBRARY_LINKAGE static)
   set(VCPKG_CRT_LINKAGE static)
@@ -14,8 +18,8 @@ if (BUILD_PLUGIN)
   vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO ianpatt/skse64
-    REF v2.1.5
-    SHA512 a084c1990b7c79ed674cae59cad02ecc72ebd5f8c75c6ad14a3c229e411f81d9aa21e6617fb1b7851d5467857acfaf22bdcce24bdf3b68af4a1fd353028eda51
+    REF v2.2.3
+    SHA512 c7c0ebc2182c2397d9f54080a7efc1296a909a08c99f63bfc4529f0126f51cc181ae5a502eddbd468ac31ca6b04c566eadb57e379ec1e35bfd28b50e4d3aa7c7
     HEAD_REF master
   )
 
@@ -24,20 +28,40 @@ if (BUILD_PLUGIN)
     PREFER_NINJA
   )
 
-  vcpkg_build_cmake()
+  vcpkg_install_cmake()
   vcpkg_copy_pdbs()
+  vcpkg_cmake_config_fixup(
+    PACKAGE_NAME skse64
+    CONFIG_PATH "lib/cmake/skse64"
+  )
 
   file(
-    INSTALL "${SOURCE_PATH}/skse64/skse64-config.cmake"
+    INSTALL "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-dbg/skse64/skse64-config.cmake"
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/skse64"
   )
 
   file(GLOB SKSE_CMAKE_FILES
-    "${SOURCE_PATH}/skse64/CMakeFiles/Export/lib/cmake/skse64/*.cmake"
+    "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-*/skse64/CMakeFiles/Export/**/*.cmake"
   )
   file(
     INSTALL ${SKSE_CMAKE_FILES}
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/skse64"
+  )
+
+  file(GLOB SKSE_COMMON_CMAKE_FILES
+    "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-*/skse64_common/CMakeFiles/Export/**/*.cmake"
+  )
+  file(
+    INSTALL ${SKSE_CMAKE_FILES}
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/skse64_common"
+  )
+
+  file(GLOB SKSE_LOADER_COMMON_CMAKE_FILES
+  "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-*/skse64_loader/skse64_loader_common/CMakeFiles/Export/**/*.cmake"
+  )
+  file(
+    INSTALL ${SKSE_LOADER_COMMON_CMAKE_FILES}
+    DESTINATION "${CURRENT_PACKAGES_DIR}/share/skse64_loader_common"
   )
 
   file(
@@ -45,13 +69,23 @@ if (BUILD_PLUGIN)
     DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
     RENAME copyright
   )
+  if (NOT INSTALL_INTERNAL_XBYAK)
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/xbyak")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/xbyak")
+  endif()
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+  file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+  # We do want the DLLs even if we're compiling static, so this is commented out
+  # if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  #   file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+  # endif()
 endif()
 
 if (INSTALL_SCRIPTS)
   vcpkg_download_distfile(SKSE_DISTRO
-    URLS "https://skse.silverlock.org/beta/skse64_2_01_05.7z"
-    FILENAME skse64_2_1_5.zip
-    SHA512 24950f5759432e1119e028b2f3972dbb983376f19856309759378c788d6794ef4d5055686dd4606c10da5cc814d6fb4812dbbc0755aadb825b8bca142b379e61
+    URLS "https://skse.silverlock.org/beta/skse64_2_02_03.7z"
+    FILENAME skse64_2_2_3.zip
+    SHA512 b57f33bf673c70b98c33d686c1bb28ce985719ab83cf4968bd0d4f9ee594126108fae1b825febab0dc3419cc19e3c72e1729d66d47367c80f406ca5e8aef9485
   )
 
   vcpkg_extract_source_archive(
@@ -59,7 +93,7 @@ if (INSTALL_SCRIPTS)
   )
 
   file(GLOB SKSE_SCRIPTS
-    "${CURRENT_BUILDTREES_DIR}/src/skse64_2_01_05/Data/Scripts/Source/*.psc"
+    "${CURRENT_BUILDTREES_DIR}/src/skse64_2_02_03/Data/Scripts/Source/*.psc"
   )
 
   file(COPY ${SKSE_SCRIPTS}
@@ -68,7 +102,7 @@ if (INSTALL_SCRIPTS)
 
   if (NOT BUILD_PLUGIN)
     file(
-      INSTALL "${CURRENT_BUILDTREES_DIR}/src/skse64_2_01_05/src/skse64/skse64_license.txt"
+      INSTALL "${CURRENT_BUILDTREES_DIR}/src/skse64_2_02_03/src/skse64/skse64_license.txt"
       DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
       RENAME copyright
     )
